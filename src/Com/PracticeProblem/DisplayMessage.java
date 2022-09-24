6c853f84dd10533136e0c73a13f7e962a4baa7bc
Prabhas3699 <prabhak620@gmail.com>
2022-09-24 14:40:57 +0530

    Displaying the message

---
+++ src/Com/PracticeProblem/DisplayMessage.java
@@ -0,0 +1,11 @@
+package Com.PracticeProblem;
+
+public class DisplayMessage {
+
+	public static void main(String[] args) {
+		// displaying the string message
+		System.out.println("Hello World");
+
+	}
+
+}
