073076b4df445391bbea58c01f555fad8cd37317
Prabhas3699 <prabhak620@gmail.com>
2022-09-24 14:46:08 +0530

    displaying the default value of primitive data types

---
+++ src/Com/PracticeProblem/PrimitiveDataTypes.java
@@ -0,0 +1,29 @@
+package Com.PracticeProblem;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		// default values of primitive data types
+		
+		//instance variables of primitive data types
+		byte myByte = 100;
+		short myShort=10000;
+		int myInt=1000000000;
+		long myLong=1000000000000000000L;
+		float myFloat=1.234f;
+		double myDouble=1.2345;
+		boolean myBool=true;
+		char myChar='P';
+		
+		System.out.println("The byte value is : "+ myByte);
+        System.out.println("The short value is : "+ myShort);
+        System.out.println("The int value is : "+ myInt);
+        System.out.println("The long value is : "+ myLong);
+        System.out.println("The float value is : "+ myFloat);
+        System.out.println("The double value is : "+ myDouble);
+        System.out.println("The boolean value is : "+ myBool);
+        System.out.println("The char value is : "+ myChar);
+
+	}
+
+}
